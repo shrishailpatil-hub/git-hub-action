329ec181390b711c4d5fd1b189b4c8ec09197546
Shrishail Patil <ff508856@gmail.com>
2026-06-20 12:50:56 +0530

    updated on trigger file

--- no-trigger-file.cpp
+++ no-trigger-file.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "this shall not trigger a action";
+    std::cout << "this shall not trigger a action becoz its a non trigger file";
     return 0;
 }
